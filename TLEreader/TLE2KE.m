61c1aa9e21dce3b858a09a89299132a327568edc
photizei <kevin.okseniuk@gmail.com>
2021-02-03 17:07:09 -0800

    Discovered bug with true anomaly calculation. Updated code and KERV.txt.

--- TLEreader/TLE2KE.m
+++ TLEreader/TLE2KE.m
@@ -41,9 +41,10 @@ mu = G * ME;
 n_km_sec = str2double(TLE.mean_motion) * 2*pi / (60*60*24) ;
 a = (mu/(n_km_sec^2))^(1/3);
 nu = rad2deg(mean2true(str2double(TLE.mean_anomaly),str2double(TLE.eccentricity), 1e-10));
-
+nu_2 = rad2deg(nuFromM(str2double(TLE.mean_anomaly),str2double(TLE.eccentricity), 1e-10));
+nu_3 = eqofcenter(str2double(TLE.mean_anomaly),str2double(TLE.eccentricity));
 %% Convert to ECI
-[rvec_ECI, vvec_ECI] = oe2eci(a, e, i, Om, w, nu);
+[rvec_ECI, vvec_ECI] = oe2eci(a, e, i, Om, w, nu_3);
 
 %% Auxiliary Functions
 % Note: all this functions were written by Kevin Okseniuk for AA279A,
@@ -215,5 +216,96 @@ f = acos((cos(E) - e)/(1 - e*cos(E)));
 if E > pi
     f = 2*pi - f;
 end
+
+  end
+
+  % Orbit eccentric anomaly, Kepler's equation keplers equation
+% Richard Rieber
+% 1/23/2005
+% rrieber@gmail.com
+% 
+% Revision 8/21/07: Fixed typo in line 38 if statement
+%                   Added H1 line for lookfor functionality
+%
+% function E = CalcEA(M,ecc,tol)
+% 
+% Purpose: Solves for eccentric anomaly, E, from a given mean anomaly, M,
+%          and eccentricty, ecc.  Performs a simple Newton-Raphson iteration
+%
+% Inputs: o M   - Mean anomaly in radians.
+%         o ecc - Eccentricity of the orbit.
+%         o tol - a tolerance at which to terminate iterations; Default
+%                 is 10^-8 radians. [OPTIONAL]
+%
+% Outputs: o E  - The eccentric anomaly in radians.
+%
+% E = CalcEA(M,ecc) uses default tolerances
+%
+% E = CalcEA(M,ecc,tol) will use a user specified tolerance, tol
+% 
+function E = CalcEA(M,ecc,tol)
+%Checking for user inputed tolerance
+if nargin == 2
+    %using default value
+    tol = 10^-8;
+elseif nargin > 3
+    error('Too many inputs.  See help CalcEA')
+elseif nargin < 2
+    error('Too few inputs.  See help CalcEA')
+end
+if (M > -pi && M < 0) || M > pi
+    E = M - ecc;
+else
+    E = M + ecc;
+end
+Etemp  = E + (M - E + ecc*sin(E))/(1-ecc*cos(E));
+Etemp2 = E;
+while abs(Etemp - Etemp2) > tol
+    Etemp = Etemp2;
+    Etemp2 = Etemp + (M - Etemp + ecc*sin(Etemp))/(1-ecc*cos(Etemp));
+end
+E = Etemp2;
+
+end
+
+% Richard Rieber
+% September 27, 2006
+% rrieber@gmail.com
+% 
+% Revision 9/25/07 - Fixed a grusome error in the default tolerance.
+%                    Changed from 10^8 radians to 10^-8 radians.  Whoops.
+%
+% function nu = nuFromM(M,ecc,tol)
+% 
+% Purpose:  This function calculates the true anomaly (nu) of a position in an orbit given
+%           the mean anomaly of the position (M) and the eccentricity (ecc) of the orbit.
+%           This uses another function, calcEA.
+%           
+% Inputs:  M - mean anomaly of position in radians
+%          ecc - eccentricity of orbit
+%          tol - A tolerance for calculating the eccentric anomaly (see help calcEA.m)
+%                Default is 10^-8 radians [OPTIONAL]
+% 
+% Output:  nu - true anomaly of position in radians
+
+function nu = nuFromM(M,ecc,tol)
+
+if nargin < 2 || nargin > 3
+    error('Incorrect number of inputs, see help nuFromM.m')
+elseif nargin == 2
+    tol = 10^-8;
+end
+
+E = CalcEA(M,ecc,tol);  %Determining eccentric anomaly from mean anomaly
+
+% Since tan(x) = sin(x)/cos(x), we can use atan2 to ensure that the angle for nu
+% is in the correct quadrant since we know both sin(nu) and cos(nu).  [see help atan2]
+nu = atan2((sin(E)*(1-ecc^2)^.5),(cos(E)-ecc));
+
+end
+
+function nu = eqofcenter(M, e)
+
+nu = M + (2*e - 0.25*(e^3))*sind(M) + (5/4)*(e^2)*sind(2*M) + (13/12)*(e^3)*sind(3*M);
 
 end
